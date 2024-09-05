8f6f0052af70f761b0e313c14dbb36848f64b3e7
Diyorbek <diyordev13@gmail.com>
2024-09-05 20:31:57 +0500

    Cmt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":iosloading"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.ios.loading)
 }
